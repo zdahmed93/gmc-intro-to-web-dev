1781dfa6a1bf2e34f06b0dfa4f603c139df52ab5
zdahmed93 <ahmedzribiahmed@gmail.com>
2019-07-25 01:50:22 +0100

    add animations and modal using jquery and jquery-modal libraries

--- js/scripts.js
+++ js/scripts.js
@@ -35,3 +35,47 @@ function changeFontFamily(event) {
     text.style.fontFamily = event.value;
 }
 
+$(".track1").mouseenter(function() {
+    $(".vmb1").css("display", "block")
+})
+
+$(".track2").mouseenter(function() {
+    $(".vmb2").css("display", "block")
+})
+
+$(".track3").mouseenter(function() {
+    $(".vmb3").css("display", "block")
+})
+
+$(".track4").mouseenter(function() {
+    $(".vmb4").css("display", "block")
+})
+
+$(".track5").mouseenter(function() {
+    $(".vmb5").css("display", "block")
+})
+
+$(".track6").mouseenter(function() {
+    $(".vmb6").css("display", "block")
+})
+
+$(".track7").mouseenter(function() {
+    $(".vmb7").css("display", "block")
+})
+
+$(".track8").mouseenter(function() {
+    $(".vmb8").css("display", "block")
+})
+
+$(".track9").mouseenter(function() {
+    $(".vmb9").css("display", "block")
+})
+
+
+$(".track").mouseleave(function() {
+    $(".view-more-button").css("display", "none")
+})
+
+$(".track, .view-more-button").click(function () {
+    $('.model-content').modal() // To use the function modal() we have to install jquery-modal library (js & css)
+})
